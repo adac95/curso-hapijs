f33decda260a43282c323a397afbc568b54c0b38
Andre del Alcazar Caceres <andredelalcazar@MacBook-Pro-de-andre.local>
2021-03-04 23:23:38 -0500

    2do commit. Implementacion de inert para trabajar una pagina estatica

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
 'use strict'
-
+const path = require('path');
+const Inert = require('@hapi/inert');
 const Hapi = require('@hapi/hapi');
 
 const init = async () => {
 
   const server = Hapi.server({
     port: 3000,
-    host: 'localhost'
+    host: 'localhost',
+    routes: {
+      files: {
+        relativeTo: path.join(__dirname, 'public')
+      }
+    },
   });
 
+  await server.register(Inert);
   server.route({
     method: 'GET',
-    path: '/',
+    path: '/home',
     handler: (request, h) => {
-      return 'Hello World'
+      return h.file('index.html')
+    }
+  })
+  server.route({
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+      directory: {
+        path: '.',
+        redirectToSlash: true,
+        index: true,
+      }
     }
   })
   await server.start();
